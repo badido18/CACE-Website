cea3f919557867942dec87f09300725bd9e3cb56
badido18 <hm_boudis@esi.dz>
2020-07-15 15:32:01 +0100

    Feat : Presentation

--- cace-website/components/landing/Presentation.js
+++ cace-website/components/landing/Presentation.js
@@ -11,8 +11,8 @@ const Presentation = () => {
             <p className={style.Text}>
             Le C.A.C.E a été fondé en 2016 par un groupe d'étudiants-artistes de l'ESI, dans le but d'alléger le stress de la vie estudiantine, ce à travers de nombreux événements et formations pertinentes aux arts et cultures diverses, organisés par ses 300+ membres tout  au long de ses 4 ans d'existence, ayant pour objectif d'encourager les étudiants à enrichir leur culture ainsi que leur donner goût à l'art sous toutes ses formes !
             </p>
-            <div className="video">
-
+            <div className={style.Video}>
+                <iframe width="975" height="521" src="https://www.youtube.com/embed/Vv4iegPHOFU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
     )
